3fb3827cb1127e6dd461cb49f8941fd7061a9b1f
Wrona10 <0marcel.wronkowski0@gmail.com>
2025-04-07 09:25:08 +0200

    testy zad. 4+6

--- KZEEMP_Tests/MachinePowerCalculatorTests.cs
+++ KZEEMP_Tests/MachinePowerCalculatorTests.cs
@@ -72,5 +72,33 @@ namespace KZEEMP_Tests
             Assert.Equal(10, result2);
             Assert.Equal(50, result3);
         }
+
+        public void MachineType_isPress_ShouldReturn5Duration()
+        {
+            //Arrange
+            MachinePowerCalculator machinePowerCalculator = new MachinePowerCalculator();
+
+            //Act
+            double result1 = machinePowerCalculator.GetPowerConsumption("Press", 1, false);
+            double result2 = machinePowerCalculator.GetPowerConsumption("Press", 4, false);
+            double result3 = machinePowerCalculator.GetPowerConsumption("Press", 10, false);
+
+            //Assert
+            Assert.Equal(7.2, result1);
+            Assert.Equal(7.2*4, result2);
+            Assert.Equal(7.2*10, result3);
+        }
+
+        public void MachineType_isUnknown_ShouldThrowException()
+        {
+            //Arrange
+            MachinePowerCalculator machinePowerCalculator = new MachinePowerCalculator();
+
+            //Act
+            ArgumentException exception = Assert.Throws<ArgumentException>(() => machinePowerCalculator.GetPowerConsumption("unknownType", 1, false));
+
+            //Assert
+            Assert.Equal("Invalid machine type.", exception.Message);
+        }
     }
 }
