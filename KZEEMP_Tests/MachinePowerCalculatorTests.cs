db226e88e5f1e5c6189be2053289328de647abbc
Wrona10 <0marcel.wronkowski0@gmail.com>
2025-04-07 09:13:53 +0200

    testy zad. 2

--- KZEEMP_Tests/MachinePowerCalculatorTests.cs
+++ KZEEMP_Tests/MachinePowerCalculatorTests.cs
@@ -29,5 +29,31 @@ namespace KZEEMP_Tests
             //Assert
             Assert.Equal("Machine type cannot be empty.", exception.Message);
         }
+
+        [Fact]
+        public void Duration_isZero_ShouldThrowException()
+        {
+            //Arrange
+            MachinePowerCalculator machinePowerCalculator = new MachinePowerCalculator();
+
+            //Act
+            ArgumentException exception = Assert.Throws<ArgumentException>(() => machinePowerCalculator.GetPowerConsumption("machineType", 0, false));
+
+            //Assert
+            Assert.Equal("Duration must be greater than zero.", exception.Message);
+        }
+
+        [Fact]
+        public void Duration_isNegative_ShouldThrowException()
+        {
+            //Arrange
+            MachinePowerCalculator machinePowerCalculator = new MachinePowerCalculator();
+
+            //Act
+            ArgumentException exception = Assert.Throws<ArgumentException>(() => machinePowerCalculator.GetPowerConsumption("machineType", -1, false));
+
+            //Assert
+            Assert.Equal("Duration must be greater than zero.", exception.Message);
+        }
     }
 }
